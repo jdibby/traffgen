221bcf2ec4656a9fa5a2492cff83e9466ed74fcd
jdibby <48450541+jdibby@users.noreply.github.com>
2024-09-19 08:46:52 -0600

    Update stager.sh
    Default creds

--- stager.sh
+++ stager.sh
@@ -9,7 +9,7 @@ apt install docker-ce git -y
 echo "### STARTING PORTAINER INSTALL ###"
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.1
-echo "### PORTAINER INSTALL COMPLETE ###"
+echo "### PORTAINER INSTALL COMPLETE WITH DEFAULT USERNAME ADMIN ###"
 
 echo "STARTING TRAFFGEN INSTALL ###"
 git clone https://github.com/jdibby/traffgen && cd traffgen
