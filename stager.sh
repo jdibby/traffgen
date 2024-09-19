051ae25fdfdca8399d998d1a139cded6553b7e15
jdibby <48450541+jdibby@users.noreply.github.com>
2024-09-19 09:09:02 -0600

    Update stager.sh
    Added Docker install

--- stager.sh
+++ stager.sh
@@ -7,7 +7,19 @@ echo "### UPDATING AND UPGRADING PACKAGES ###"
 apt update -y && apt upgrade -y
 
 echo "### INSTALLING DOCKER AND GIT ###"
-apt install docker-ce git -y
+# Add Docker's official GPG key:
+apt-get install ca-certificates curl git
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/raspbian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/raspbian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 echo "### STARTING PORTAINER INSTALL ###"
 docker volume create portainer_data
