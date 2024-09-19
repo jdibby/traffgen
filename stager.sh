989cda932fe84f70fb228ddf4ff06cb7259185ba
jdibby <48450541+jdibby@users.noreply.github.com>
2024-09-19 11:58:36 -0600

    Update stager.sh
    Bold styling to script

--- stager.sh
+++ stager.sh
@@ -11,6 +11,7 @@ if [ $WHOAREYOU != root ]; then
    exit
 fi
 
+### Create Bold and Normal Font Style ###
 BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
 
@@ -25,7 +26,7 @@ echo "${BOLD}### UPDATING AND UPGRADING PACKAGES COMPLETE ###${NORMAL}"
 
 echo -e -n "\n" 
 
-echo "### STARTING DOCKER AND GIT INSTALL ###"
+echo "${BOLD}### STARTING DOCKER AND GIT INSTALL ###${NORMAL}"
 # Add Docker's official GPG key:
 apt-get install ca-certificates curl git
 install -m 0755 -d /etc/apt/keyrings
@@ -39,11 +40,11 @@ echo \
 sudo apt-get update -y
 
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-echo "### INSTALLATION OF DOCKER AND GIT COMPLETE ###"
+echo "${BOLD}### INSTALLATION OF DOCKER AND GIT COMPLETE ###${NORMAL}"
 
 echo -e -n "\n" 
 
-echo "### STARTING PORTAINER INSTALL ###"
+echo "${BOLD}### STARTING PORTAINER INSTALL ###${NORMAL}"
 ### Cleanup potential existing Portainer installs ###
 docker stop portainer
 docker rm portainer
@@ -52,11 +53,11 @@ docker rmi $(docker images | grep 'portainer')
 
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.1
-echo "### PORTAINER INSTALL COMPLETE WITH DEFAULT USERNAME ADMIN ###"
+echo "${BOLD}### PORTAINER INSTALL COMPLETE WITH DEFAULT USERNAME ADMIN ###${NORMAL}"
 
 echo -e -n "\n" 
 
-echo "### STARTING TRAFFGEN INSTALL ###"
+echo "${BOLD}### STARTING TRAFFGEN INSTALL ###${NORMAL}"
 ### Cleanup potential existing traffgen installs ###
 docker ps | grep jdibby/traffgen | awk '{print $1}' | xargs docker stop
 rm -rf $HOMEDIR/traffgen/
@@ -64,19 +65,19 @@ rm -rf $HOMEDIR/traffgen/
 git clone https://github.com/jdibby/traffgen
 cd $HOMEDIR/traffgen/
 docker build -t jdibby/traffgen $HOMEDIR/traffgen/.
-echo "### TRAFFGEN INSTALL COMPLETE ###"
+echo "${BOLD}### TRAFFGEN INSTALL COMPLETE ###${NORMAL}"
 
 echo -e -n "\n" 
 
-echo "### STARTING TRAFFGEN ###"
+echo "${BOLD}### STARTING TRAFFGEN ###${NORMAL}"
 docker run --detach --restart unless-stopped jdibby/traffgen:latest
-echo "### TRAFFGEN STARTED ###"
+echo "${BOLD}### TRAFFGEN STARTED ###${NORMAL}"
 
 echo -e -n "\n" 
 
-echo "### FINAL CLEANUP STARTED ###"
+echo "${BOLD}### FINAL CLEANUP STARTED ###${NORMAL}"
 rm -rf $HOMEDIR/traffgen
 rm -rf $HOMEDIR/stager.sh
-echo "### FINAL CLEANUP COMPLETE ###"
+echo "${BOLD}### FINAL CLEANUP COMPLETE ###${NORMAL}"
 
 echo -e -n "\n" 
