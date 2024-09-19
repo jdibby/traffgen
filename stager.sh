1ceef61da64e72faaf23c1e508092f6ebd5ed6c5
jdibby <48450541+jdibby@users.noreply.github.com>
2024-09-19 09:11:25 -0600

    Update stager.sh

--- stager.sh
+++ stager.sh
@@ -22,7 +22,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/raspbian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
