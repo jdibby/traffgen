bd5b4f3b979b0015ad9d56a15ddf997da975ee16
jdibby <48450541+jdibby@users.noreply.github.com>
2024-09-19 08:29:27 -0600

    Create stager.sh
    Stager script for RPI's

---
+++ stager.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "### UPDATING AND UPGRADING PACKAGES ###"
+apt update -y && apt upgrade -y
+
+echo "INSTALLING DOCKER"
+apt install docker-ce -y
+
+echo "### STARTING PORTAINER INSTALL ###"
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/do
+cker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.1
+echo "### PORTAINER INSTALL COMPLETE ###"
+
+echo "STARTING TRAFFGEN INSTALL ###"
+git clone https://github.com/jdibby/traffgen
+cd traffgen
+docker build -t jdibby/traffgen .
+docker run --detach --restart unless-stopped jdibby/traffgen:latest
+echo "### TRAFFGEN INSTALL COMPLETE ###"
