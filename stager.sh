da0d4be1b3d231691de52b7994a2bd999453056d
jdibby <48450541+jdibby@users.noreply.github.com>
2024-09-19 09:19:04 -0600

    Update stager.sh
    Cleanup Portainer

--- stager.sh
+++ stager.sh
@@ -27,6 +27,12 @@ sudo apt-get update -y
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 echo "### STARTING PORTAINER INSTALL ###"
+### Cleanup potential existing Portainer installs ###
+docker stop portainer
+docker rm portainer
+docker volume rm portainer_data
+docker rmi $(docker images | grep 'portainer')
+
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.1
 echo "### PORTAINER INSTALL COMPLETE WITH DEFAULT USERNAME ADMIN ###"
