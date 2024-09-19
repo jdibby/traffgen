1d39d8b6b870302a0d51f770f072f1865f0c0663
jdibby <48450541+jdibby@users.noreply.github.com>
2024-09-19 11:52:36 -0600

    Update stager.sh

--- stager.sh
+++ stager.sh
@@ -14,20 +14,18 @@ fi
 BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
 
-echo "this is ${bold}bold${normal} but this isn't"
-
 ### SET HOME DIRECTORY ###
 HOMEDIR=`pwd`
 
 echo -e -n "\n" 
 
-$BOLD
+${BOLD}
 echo "### UPDATING AND UPGRADING PACKAGES ###"
-$NORMAL
+${NORMAL}
 apt update -y && apt upgrade -y
-$BOLD
+${BOLD}
 echo "### UPDATING AND UPGRADING PACKAGES COMPLETE """
-$NORMAL
+${NORMAL}
 
 echo -e -n "\n" 
 
