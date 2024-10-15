3096d337a88cc3930eea7765603a1d6ed88348a9
jdibby <48450541+jdibby@users.noreply.github.com>
2024-10-15 12:35:29 -0600

    Fixed docker install for Ubuntu

--- stager.sh
+++ stager.sh
@@ -83,8 +83,8 @@ if [ -n "$RPIVER" ] && [ "$RPIVER" -lt 5 ]; then
 elif [ -n "$RPIVER" ] && [ "$RPIVER" -eq 5 ]; then
    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
    # Add Docker's official GPG key:
-   apt-get update
-   apt-get install ca-certificates curl
+   apt-get update -y
+   apt-get install ca-certificates curl -y
    install -m 0755 -d /etc/apt/keyrings
    curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
    chmod a+r /etc/apt/keyrings/docker.asc
@@ -99,8 +99,8 @@ elif [ -n "$RPIVER" ] && [ "$RPIVER" -eq 5 ]; then
 elif [ "$UBUNTU" -gt 0 ]; then
    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
    # Add Docker's official GPG key:
-   apt-get update
-   apt-get install ca-certificates curl
+   apt-get update -y
+   apt-get install ca-certificates curl -y
    install -m 0755 -d /etc/apt/keyrings
    curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
    chmod a+r /etc/apt/keyrings/docker.asc
@@ -110,7 +110,7 @@ elif [ "$UBUNTU" -gt 0 ]; then
    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
    apt-get update -y
-   apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+   apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
    systemctl restart docker
 elif [ "$ROCKY" -gt 0 ]; then
    dnf remove -y docker-ce docker-ce-cli containerd.io
