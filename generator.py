b96182dab52a7a18c811ad5e8d11c82abb87c724
jdibby <48450541+jdibby@users.noreply.github.com>
2024-10-15 09:22:00 -0600

    TEST

--- generator.py
+++ generator.py
@@ -108,7 +108,7 @@ def http_random():
             print (Fore.BLACK)
             print (Back.GREEN + "##############################################################")
             print (Style.RESET_ALL)
-            #print ("Testing HTTP: (%d of %d): %s" %((count_urls+1), target_urls, url))
+            print ("Testing HTTP: (%d of %d): %s" %((count_urls+1), target_urls, url))
             print (f"Agent: {user_agent}")
             print (Fore.BLACK)
             print (Back.GREEN + "##############################################################")
@@ -130,15 +130,15 @@ def http_download_zip():
     elif ARGS.size == 'XL':
         target = '1GB'
         cmd = f'curl --limit-rate 3M --insecure  --show-error --connect-timeout 5 -L -o /dev/null -A {user_agent} https://link.testfile.org/'
-    print (Fore.BLACK)
+    """print (Fore.BLACK)
     print (Back.GREEN + "##############################################################")
     print (Style.RESET_ALL)
-    #print ("Testing HTTP: Download %s ZIP File" %(target))
+    print ("Testing HTTP: Download %s ZIP File" %(target))
     print (f"Agent: {user_agent}")
     print (Fore.BLACK)
     print (Back.GREEN + "##############################################################")
     print (Style.RESET_ALL)
-    subprocess.call(cmd, shell=True)
+"""    subprocess.call(cmd, shell=True)
 
 def http_download_targz():
     cmd = 'curl --limit-rate 3M --insecure  --show-error --connect-timeout 5 -o /dev/null http://wordpress.org/latest.tar.gz'
