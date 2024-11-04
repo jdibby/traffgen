493feb193542f4b69ec70dd1509ac3c7634258e5
jdibby <48450541+jdibby@users.noreply.github.com>
2024-11-04 14:22:19 -0700

    Update stager.sh
    More Docker installation logic

--- stager.sh
+++ stager.sh
@@ -73,71 +73,89 @@ fi
 echo -e -n "\n" 
 echo "${BOLD}### OPERATING SYSTEM DETECTED AND UPDATES COMPLETED ###${NORMAL}"
 echo -e -n "\n" 
-echo "${BOLD}### STARTING DOCKER INSTALL ###${NORMAL}"
-echo -e -n "\n" 
-### Different installation options for different OS' ###
-if [ -n "$RPIVER" ] && [ "$RPIVER" -lt 5 ]; then
-   for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-   # Add Docker's official GPG key:
-   apt-get install ca-certificates curl git -y
-   install -m 0755 -d /etc/apt/keyrings
-   curl -fsSL https://download.docker.com/linux/raspbian/gpg -o /etc/apt/keyrings/docker.asc
-   chmod a+r /etc/apt/keyrings/docker.asc
-   echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/raspbian \
-      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-   apt-get update -y
-   apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-   systemctl restart docker
-elif [ -n "$RPIVER" ] && [ "$RPIVER" -eq 5 ]; then
-   for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-   # Add Docker's official GPG key:
-   apt-get update -y
-   apt-get install ca-certificates curl -y
-   install -m 0755 -d /etc/apt/keyrings
-   curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-   chmod a+r /etc/apt/keyrings/docker.asc
-   # Add the repository to Apt sources:
-   echo \
-   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-   tee /etc/apt/sources.list.d/docker.list > /dev/null
-   apt-get update -y
-   apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-   systemctl restart docker
-elif [ "$UBUNTU" -gt 0 ]; then
-   for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-   # Add Docker's official GPG key:
-   apt-get update -y
-   apt-get install ca-certificates curl -y
-   install -m 0755 -d /etc/apt/keyrings
-   curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-   chmod a+r /etc/apt/keyrings/docker.asc
-   # Add the repository to Apt sources:
-   echo \
-   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-   apt-get update -y
-   apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-   systemctl restart docker
-elif [ "$ROCKY" -gt 0 ]; then
-   dnf remove -y docker-ce docker-ce-cli containerd.io
-   rm -rf /var/lib/docker
-   rm -rf /var/lib/containerd
-   dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-   dnf update -y
-   dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-   systemctl start docker
-   systemctl enable docker
-else
-   echo "### YOU ARE RUNNING AN UNSUPPORTED OPERATING SYSTEM ###"
-   echo -e -n "\n" 
-   exit 1
+if [ -x "$(command -v docker)" ]; then
+    echo -e -n "\n" 
+    echo "${BOLD}### DOCKER IS ALREADY INSTALLED AND IS READY TO USE ###${NORMAL}"
+    echo -e -n "\n" 
+    else
+        echo "${BOLD}### STARTING DOCKER INSTALL ###${NORMAL}"
+        echo -e -n "\n" 
+        ### Different installation options for different OS' ###
+        if [ -n "$RPIVER" ] && [ "$RPIVER" -lt 5 ]; then
+            for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+            # Add Docker's official GPG key:
+            apt-get install ca-certificates curl git -y
+            install -m 0755 -d /etc/apt/keyrings
+            curl -fsSL https://download.docker.com/linux/raspbian/gpg -o /etc/apt/keyrings/docker.asc
+            chmod a+r /etc/apt/keyrings/docker.asc
+            echo \
+                "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/raspbian \
+                $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+                sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+            apt-get update -y
+            apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+            systemctl restart docker
+            echo -e -n "\n" 
+            echo "${BOLD}### DOCKER INSTALLED AND IS READY TO USE ###${NORMAL}"
+            echo -e -n "\n" 
+        elif [ -n "$RPIVER" ] && [ "$RPIVER" -eq 5 ]; then
+            for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+            # Add Docker's official GPG key:
+            apt-get update -y
+            apt-get install ca-certificates curl -y
+            install -m 0755 -d /etc/apt/keyrings
+            curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+            chmod a+r /etc/apt/keyrings/docker.asc
+            # Add the repository to Apt sources:
+            echo \
+            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+            $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+            tee /etc/apt/sources.list.d/docker.list > /dev/null
+            apt-get update -y
+            apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+            systemctl restart docker
+            echo -e -n "\n" 
+            echo "${BOLD}### DOCKER INSTALLED AND IS READY TO USE ###${NORMAL}"
+            echo -e -n "\n" 
+        elif [ "$UBUNTU" -gt 0 ]; then
+            for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+            # Add Docker's official GPG key:
+            apt-get update -y
+            apt-get install ca-certificates curl -y
+            install -m 0755 -d /etc/apt/keyrings
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+            chmod a+r /etc/apt/keyrings/docker.asc
+            # Add the repository to Apt sources:
+            echo \
+            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+            $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+            sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+            apt-get update -y
+            apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+            systemctl restart docker
+            echo -e -n "\n" 
+            echo "${BOLD}### DOCKER INSTALLED AND IS READY TO USE ###${NORMAL}"
+            echo -e -n "\n" 
+        elif [ "$ROCKY" -gt 0 ]; then
+            dnf remove -y docker-ce docker-ce-cli containerd.io
+            rm -rf /var/lib/docker
+            rm -rf /var/lib/containerd
+            dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+            dnf update -y
+            dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+            systemctl start docker
+            systemctl enable docker
+            echo -e -n "\n" 
+            echo "${BOLD}### INSTALLATION OF DOCKER COMPLETE ###${NORMAL}"
+            echo -e -n "\n" 
+        else
+            echo -e -n "\n" 
+            echo "### YOU ARE RUNNING AN UNSUPPORTED OPERATING SYSTEM ###"
+            echo -e -n "\n" 
+            exit 1
+        fi
 fi
 
-echo -e -n "\n" 
 echo "${BOLD}### INSTALLATION OF DOCKER COMPLETE ###${NORMAL}"
 echo -e -n "\n" 
 
