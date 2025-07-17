c6b343860049a95765845f38c900794e41caae82
jdibby <48450541+jdibby@users.noreply.github.com>
2025-07-16 23:48:24 -0600

    Added nointeractive support to Nikto

--- generator.py
+++ generator.py
@@ -246,7 +246,7 @@ def web_scanner():
     random.shuffle(webscan_endpoints)
     url = webscan_endpoints[0]
     
-    cmd = f"nikto.pl -h '{url}' -maxtime '{timeout}' -timeout 1"
+    cmd = f"nikto.pl -h '{url}' -maxtime '{timeout}' -timeout 1 -nointeractive"
     
     print (Fore.BLACK)
     print (Back.GREEN + "##############################################################")
