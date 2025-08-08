cfdc1d3e0484a62d9378076737b648da9f67c321
jdibby <48450541+jdibby@users.noreply.github.com>
2025-08-07 21:37:28 -0600

    More cleanup

--- generator.py
+++ generator.py
@@ -1080,7 +1080,6 @@ def scrape_single_link(url):
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)',
     ]
-
     sleep(random.uniform(0.2, 2))
     random.shuffle(user_agents)
 
